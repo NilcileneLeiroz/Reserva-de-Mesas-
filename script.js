7a54564205c41b46f8f093be5908eb04019f4aa6
NilcileneLeiroz <nilleiroz@icloud.com>
2025-05-26 23:46:12 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,5 @@
 
 document.getElementById('reservaForm').addEventListener('submit', function(e) {
     e.preventDefault();
-    const nome = document.getElementById('nome').value;
-    const telefone = document.getElementById('telefone').value;
-    const cidade = document.getElementById('cidade').value;
-    const pessoas = document.getElementById('pessoas').value;
-    const area = document.getElementById('area').value;
-
-    const item = document.createElement('p');
-    item.textContent = `${nome.toUpperCase()} (${pessoas} pessoas - ${cidade.toUpperCase()})`;
-
-    document.getElementById('listaEspera').appendChild(item);
-
-    document.getElementById('reservaForm').reset();
+    alert('Reserva enviada com sucesso!');
 });
-
-function liberarMesa() {
-    document.getElementById('statusMesa').textContent = "Mesa disponível";
-}
